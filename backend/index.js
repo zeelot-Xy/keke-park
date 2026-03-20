3307ddeb206ae7c25a59e87cc85653d58c135f88
zeelot-Xy <zeelot.godwin@gmail.com>
2026-03-20 12:58:55 +0100

    feat:connected driver routes to server and enabled static serving of upload files

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,11 @@ app.get("/test-db", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
+// Authentication routing
 const authRoutes = require("./routes/auth");
-
 app.use("/api/auth", authRoutes);
+// Driver routing
+const driverRoutes = require("./routes/driver");
+app.use("/api/driver", driverRoutes);
+// Upload routing
+app.use("/uploads", express.static("uploads"));
